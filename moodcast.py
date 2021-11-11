72542663d7ca2efac124c4a652860a0205b62084
Laurens van Helvoort <l.f.a.vanhelvoort@student.utwente.nl>
2021-11-11 22:47:06 +0100

    Help button added, option to view weekly report with recorded moods, average and visualizations. Visual upgrade to bar chart, and better trend/moving average line for regular line graph. Overall structure has been improved.

--- moodcast.py
+++ moodcast.py
@@ -14,22 +14,11 @@ from matplotlib.figure import Figure
 from matplotlib.ticker import MaxNLocator
 from scipy.interpolate import interp1d
 
-"""
-TO DO 
-- option to choose reminder
-- display day / week of year
-- weekly report
-    + average mood
-    + bar chart
-    +
-
-- link to other users/friends, see (certain) data from them and contact options of person is not well
-"""
-
 days_counter = []
 mood_list = []
 mood_options = ["awful", "bad", "meh", "good", "amazing"]
 bg_col = "#1e1e1e"
+acc_col = '#5E10BC'
 
 root = Tk()
 root.geometry("750x450")
@@ -67,91 +56,6 @@ def register_mood(current_mood):
         if answer: weekly_report()
 
 
-def show_graph():
-    global mood_list
-    global days_counter
-
-    # Display graph
-    if mood_list:
-        if len(mood_list) > 3:
-            make_graph(days_counter, mood_list)
-        else:
-            messagebox.showwarning(title="Warning", message="Wait for day 4 to see graph")
-    else:
-        messagebox.showwarning(title="Warning", message="You have no registered moods yet!")
-
-
-def make_graph(days, moods):
-    graph_screen = Tk()
-    graph_screen.title("Mood progress")
-    graph_screen.resizable(False, False)
-
-    # Compile lists into array
-    x = np.array(days)
-    y = np.array(moods)
-
-    # Use cubic interpolation model for smoothing the graph
-    cubic_interpolation_model = interp1d(x, y, kind="cubic")
-    x_smooth = np.linspace(x.min(), x.max(), 150)
-    y_smooth = cubic_interpolation_model(x_smooth)
-
-    # Plot data
-    fig = Figure(figsize=(5, 5), dpi=120)
-    fig.patch.set_facecolor('lightgrey')
-    ax = fig.add_subplot(111)
-
-    # Round x-axis values
-    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-
-    # Color bands on y-axis
-    ax.axhspan(0, 1, alpha=0.35, color="red")
-    ax.axhspan(1, 2, alpha=0.35, color="orange")
-    ax.axhspan(2, 3, alpha=0.35, color="yellow")
-    ax.axhspan(3, 4, alpha=0.35, color="lightgreen")
-    ax.axhspan(4, 5.25, alpha=0.35, color="limegreen")
-
-    # Set labels and title
-    ax.set_ylim([0, 5.25])
-    ax.set_yticks([0, 1, 2, 3, 4, 5])
-    ax.set_yticklabels(("", "Awful", "Bad", "Meh", "Good", "Amazing"), fontsize=8.5)
-    ax.title.set_text("My progress")
-    ax.set_xlabel("Days")
-    ax.set_ylabel("Mood")
-
-    # Plot graph
-    z = np.polyfit(x_smooth, y_smooth, 1)
-    p = np.poly1d(z)
-    ax.plot(x, p(x), "r--", alpha=0.25)
-    ax.plot(x_smooth, y_smooth, color='yellow', linewidth=3, solid_capstyle='round')
-
-    # Draw graph to the screen
-    canvas = FigureCanvasTkAgg(fig, master=graph_screen)
-    canvas.draw()
-    canvas.get_tk_widget().pack()
-
-
-def update_current_day():
-    # Set current_day label to the correct day
-    current_day.set(days_counter[-1] if days_counter else 0)
-
-
-def make_bar_chart():
-    bar_chart_screen = Tk()
-
-    # Get data from last 7 days
-    x = days_counter[-7:]
-    y = mood_list[-7:]
-
-    fig = Figure(figsize=(7, 5), dpi=120)
-    fig.patch.set_facecolor('lightgrey')
-    ax = fig.add_subplot(111)
-    ax.bar(x, y, color='#5E10BC')
-
-    canvas = FigureCanvasTkAgg(fig, master=bar_chart_screen)
-    canvas.draw()
-    canvas.get_tk_widget().pack()
-
-
 def save_progress():
     # Save data in pickle files in directory
     with open('mood_data.pkl', 'wb') as f:
@@ -172,6 +76,104 @@ def load_progress():
 
         with open('days_data.pkl', 'rb') as f:
             days_counter = pickle.load(f)
+
+
+def show_graph():
+    global mood_list
+    global days_counter
+
+    # Display graph
+    if mood_list:
+        if len(mood_list) > 3:
+            make_graph(days_counter, mood_list)
+        else:
+            messagebox.showwarning(title="Warning", message="Wait for day 4 to see graph")
+    else:
+        messagebox.showwarning(title="Warning", message="You have no registered moods yet!")
+
+
+def make_graph(days, moods):
+    graph_screen = Tk()
+    graph_screen.title("Mood progress")
+    graph_screen.resizable(False, False)
+    graph_screen.iconbitmap('img/moodcast_icon.ico')
+
+    # Compile lists into array
+    x = np.array(days)
+    y = np.array(moods)
+
+    # Use cubic interpolation model for smoothing the graph
+    cubic_interpolation_model = interp1d(x, y, kind="cubic")
+    x_smooth = np.linspace(x.min(), x.max(), 150)
+    y_smooth = cubic_interpolation_model(x_smooth)
+
+    # Create widget
+    fig = Figure(figsize=(5, 5), dpi=120)
+    fig.patch.set_facecolor('lightgrey')
+    ax = fig.add_subplot(111)
+
+    # Round x-axis values
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # Color bands on y-axis
+    ax.axhspan(0, 1, alpha=0.35, color="red")
+    ax.axhspan(1, 2, alpha=0.35, color="orange")
+    ax.axhspan(2, 3, alpha=0.35, color="yellow")
+    ax.axhspan(3, 4, alpha=0.35, color="lightgreen")
+    ax.axhspan(4, 5.25, alpha=0.35, color="limegreen")
+
+    # Graph styling
+    ax.set_ylim([0, 5.25])
+    ax.set_yticks([0, 1, 2, 3, 4, 5])
+    ax.set_yticklabels(("", "Awful", "Bad", "Meh", "Good", "Amazing"), fontsize=8.5)
+    ax.title.set_text("My progress")
+    ax.set_xlabel("Day")
+    ax.set_ylabel("Mood")
+
+    # Plot curved trend line
+    curvature = 2 if len(x) > 6 else 1
+    z = np.polyfit(x, y, curvature)
+    p = np.poly1d(z)
+    ax.plot(x, p(x), "r--", alpha=0.25)
+
+    # Plot graph
+    ax.plot(x_smooth, y_smooth, color='yellow', linewidth=3, solid_capstyle='round')
+
+    # Draw graph to the screen
+    canvas = FigureCanvasTkAgg(fig, master=graph_screen)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+
+
+def make_bar_chart():
+    bar_chart_screen = Tk()
+    bar_chart_screen.title("Weekly report")
+    bar_chart_screen.geometry("600x550")
+    bar_chart_screen.resizable(False, False)
+    bar_chart_screen.iconbitmap('img/moodcast_icon.ico')
+
+    # Get data from last 7 days
+    x = days_counter[-7:]
+    y = mood_list[-7:]
+
+    # Create widget
+    fig = Figure(figsize=(7, 5), dpi=120)
+    fig.patch.set_facecolor('lightgrey')
+    ax = fig.add_subplot(111)
+
+    # Graph styling
+    ax.set_ylim([0, 5.25])
+    ax.set_yticks([0, 1, 2, 3, 4, 5])
+    ax.set_yticklabels(("", "Awful", "Bad", "Meh", "Good", "Amazing"), fontsize=8.5)
+    ax.title.set_text("My week")
+    ax.set_xlabel("Day")
+
+    # Plot bar chart
+    ax.bar(x, y, color=acc_col)
+
+    canvas = FigureCanvasTkAgg(fig, master=bar_chart_screen)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
 
 
 def clear_history():
@@ -200,6 +202,11 @@ def clear_history():
         messagebox.showwarning(title="Warning", message="You have no mood history yet!")
 
 
+def update_current_day():
+    # Set current_day label to the correct day
+    current_day.set(days_counter[-1] if days_counter else 0)
+
+
 def show_info():
     messagebox.showinfo(title="How does this work?",
                         message="This program helps you track your mood! \n\n"
@@ -215,30 +222,43 @@ def weekly_report():
     weekly_report_screen = Tk()
     weekly_report_screen.title("My weekly report")
     weekly_report_screen.geometry("450x650")
-    weekly_report_screen.configure(bg=bg_col)
+    weekly_report_screen.resizable(False, False)
     weekly_report_screen.iconbitmap('img/moodcast_icon.ico')
+    weekly_report_screen.configure(bg=bg_col)
 
     # Get scores from last 7 days and convert to moods
     weekly_scores = mood_list[-7:]
     weekly_moods = [mood_options[score - 1] for score in weekly_scores]
 
+    # Banner
+    Label(weekly_report_screen, text="", font=('Segoe UI Bold', 10), bg=acc_col, fg='white').pack(fill=X,
+                                                                                                    anchor='center')
+
     # Calculate average mood
-    avg = mood_options[round((mean(weekly_scores))) - 1]
+    avg_score = round(mean(weekly_scores)) - 1
+    avg_mood = mood_options[avg_score]
 
     # Display average mood
-    Label(weekly_report_screen, text=f"Your average mood this week: {avg}", font=('Segoe UI Bold', 15), bg=bg_col,
-          fg='white').place(relx=0.5, rely=0.125, anchor=CENTER)
+    Label(weekly_report_screen, text=f"On average, you felt {avg_mood} this week", font=('Segoe UI Bold', 17),
+          bg=bg_col,
+          fg='white').place(relx=0.5, rely=0.07, anchor=CENTER)
+
+    # Display encouraging comment
+    comment = "You're doing great, keep it up!" if avg_score > 2 else "Keep your head up, next week will be awesome!"
+    Label(weekly_report_screen, text=comment, font=('Segoe UI Bold', 12), bg=bg_col, fg='darkgrey').place(relx=0.5,
+                                                                                                          rely=0.135,
+                                                                                                          anchor=CENTER)
 
     # Bar chart button
     Button(weekly_report_screen, text="Show this week's progress", command=make_bar_chart,
-           font=('Segoe UI Bold', 13), bg='#5E10BC', fg='white', padx=8, pady=8) \
+           font=('Segoe UI Bold', 13), bg=acc_col, fg='white', padx=8, pady=8) \
         .place(relx=0.5, rely=0.22, anchor=CENTER)
 
     # Place days with the appropriate moods
     for count, mood in enumerate(weekly_moods):
         Label(weekly_report_screen, text=f"Day {count + 1}:", font=('Segoe UI Bold', 20), bg=bg_col, fg='white') \
             .place(relx=0.3, rely=(count + 3) * spacing * 0.68, anchor=CENTER)
-        Label(weekly_report_screen, text=mood, font=('Segoe UI Bold', 20), bg=bg_col, fg='#5E10BC') \
+        Label(weekly_report_screen, text=mood, font=('Segoe UI Bold', 20), bg=bg_col, fg=acc_col) \
             .place(relx=0.7, rely=(count + 3) * spacing * 0.68, anchor=CENTER)
 
 
@@ -267,15 +287,15 @@ current_day = StringVar()
 update_current_day()
 
 # Tkinter elements
-graph_btn = Button(root, text="Show progress", command=show_graph, font=font, bg='#5E10BC', fg='white', padx=8, pady=8) \
+graph_btn = Button(root, text="Show progress", command=show_graph, font=font, bg=acc_col, fg='white', padx=8, pady=8) \
     .place(relx=0.5, rely=0.8, anchor=CENTER)
-banner_lbl = Label(root, text="Moodcast", font=('Segoe UI Bold', 32), bg='#5E10BC', fg='white') \
+banner_lbl = Label(root, text="Moodcast", font=('Segoe UI Bold', 32), bg=acc_col, fg='white') \
     .pack(fill=X, anchor='center')
 date_lbl = Label(root, text=str(date.today().strftime("%B %d, %Y")), font=('Segoe UI Bold', 18), bg=bg_col,
                  fg='darkgrey').pack(anchor='center')
 day_lbl = Label(root, textvariable=current_day, font=('Segoe UI Bold', 18), bg=bg_col,
-                fg='darkgrey').pack(anchor='center')
-text_lbl = Label(root, text="How are you feeling today?", font=('Segie UI Bold', 26), bg=bg_col, fg='white').place(
+                fg=acc_col).pack(anchor='center')
+text_lbl = Label(root, text="How are you feeling today?", font=('Segoe UI Bold', 26), bg=bg_col, fg='white').place(
     relx=0.5, rely=0.35, anchor=CENTER)
 clear_btn = Button(root, text="Clear history", command=clear_history, font=font, bg='darkgrey', fg='black').place(
     rely=1, relx=1, x=-7, y=-7, anchor=SE)
